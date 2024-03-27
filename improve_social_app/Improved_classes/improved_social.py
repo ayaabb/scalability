89c9f533bc342e08bb84095e58d804349d08a581
ayaabb <ayaabbas011@gmail.com>
2024-03-28 00:23:24 +0200

    added docstring and runtime complexity

--- improve_social_app/Improved_classes/improved_social.py
+++ improve_social_app/Improved_classes/improved_social.py
@@ -2,26 +2,41 @@ from Improved_classes.improved_user import ImprovedUser
 
 
 class ImprovedSocialMediaPlatform:
+    """Represents an improved social media platform with efficient user management.
+    Attributes:
+        users (dict): A dictionary containing usernames as keys and ImprovedUser objects as values.
+    """
+
     def __init__(self):
         self.users = {}
 
-    def register_user(self, username):  # o(1) average
+    def register_user(self, username):  # O(1) average - Constant time for dictionary access
+        """Registers a new user with the given username.
+        param: username (str): The username of the user to register.
+        """
         if self.users.get(username) is None:
             self.users[username] = ImprovedUser(username)
 
-    def get_user_by_username(self, username):  # o(1) average
+    def get_user_by_username(self, username):  # O(1) average - Constant time for dictionary access
+        """Retrieves the user object associated with the given username.
+        param:  username (str): The username of the user to retrieve.
+        Returns: ImprovedUser or None: The user object if found, None otherwise.
+        """
         if self.users.get(username) is not None:
             return self.users[username]
-        print(username)
         return None
 
-    def generate_timeline(self, username):  # o(n*m)
+    def generate_timeline(self, username):  # O(n*m) -- Where 'n' is the number of users the given user follows and 'm'
+        # is the average number of posts per followed user
+        """Generates a timeline of posts from followed users for the given username.
+        param: username (str): The username of the user for whom to generate the timeline.
+        Returns: list: A list of posts from followed users.
+        """
         user = self.get_user_by_username(username)
         if not user:
             return []
         timeline = []
         for followed_user in user.following:
             followed_user = self.get_user_by_username(followed_user)
-
             timeline += followed_user.posts
         return timeline
--- improve_social_app/classes/social_media.py
+++ improve_social_app/classes/social_media.py
@@ -2,27 +2,51 @@ from classes.user import User, posts
 
 
 class SocialMediaPlatform:
+    """Represents a social media platform.
+    Attributes:
+        users (list): A list of User objects representing registered users.
+    """
+
     def __init__(self):
         self.users = []
 
-    def register_user(self, username):  # o(n)
+    def register_user(self, username):
+        """Registers a new user with the given username.
+        param: username (str): The username of the user to register.
+        """
+        # O(n) - Linear time complexity due to iteration over the list of users (_is_username_taken method)
         if not self._is_username_taken(username):
             user = User(username)
             self.users.append(user)
 
-    def _is_username_taken(self, username):  # o(n)
+    def _is_username_taken(self, username):
+        """Checks if the given username is already taken.
+        param: username (str): The username to check.
+        Returns: bool: True if the username is taken, False otherwise.
+        """
+        # O(n) - Linear time complexity due to iteration over the list of users
         for user in self.users:
             if user.username == username:
                 return True
         return False
 
-    def get_user_by_username(self, username):  # o(n)
+    def get_user_by_username(self, username):
+        """Retrieves the user object associated with the given username.
+        param: username (str): The username of the user to retrieve.
+        Returns: User or None: The user object if found, None otherwise.
+        """
+        # O(n) - Linear time complexity due to iteration over the list of users
         for user in self.users:
             if user.username == username:
                 return user
         return None
 
-    def generate_timeline(self, username):  # o(n^2)
+    def generate_timeline(self, username):
+        """Generates a timeline of posts from followed users for the given username.
+        param: username (str): The username of the user for whom to generate the timeline.
+        Returns: list: A list of posts from followed users.
+        """
+        # O(n^2) - Quadratic time complexity due to nested iteration over users and posts
         user = self.get_user_by_username(username)
         if not user:
             return []
